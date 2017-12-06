0be165db6bc7ef4e2798859b4aea2deb5f3be6e8
huygo93123 <huydk1@gmail.com>
2017-12-06 15:49:30 +0700

    update

--- resources/views/vendor/adminlte/order/index.blade.php
+++ resources/views/vendor/adminlte/order/index.blade.php
@@ -1,22 +1,14 @@
 @extends('adminlte::layouts.backend')
 @section('htmlheader')
     @parent
-    <link rel="icon" href="assets/img/favicon.png">
-    <!-- Bootstrap core CSS     -->
-    <link href="/presale/assets/css/bootstrap.min.css" rel="stylesheet" />
-    <!--  Material Dashboard CSS    -->
-    <link href="/presale/assets/css/material-dashboard.css" rel="stylesheet" />
     <!--  CSS for Demo Purpose, don't include it in your project     -->
     <link href="/presale/assets/css/demo.css" rel="stylesheet" />
     <!--  Custom CSS    -->
     <link href="/presale/assets/css/custom.css" rel="stylesheet" />
-    <!--     Fonts and icons     -->
-    <!-- <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet"> -->
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 @endsection
 @section('content')
-    <div class="container-fluid">
+    <div class="content">
+         <div class="container-fluid">
         <div class="row d-flex" section="dashboard-status">
             <div class="col-md-8 align-self-center">
                 <div class="row">
@@ -253,78 +245,9 @@
             </div>
         </div>
     </div>
+    </div>
 @endsection
 @section('script')
-    <!--   Core JS Files   -->
-    <script src="/presale/assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js"></script>
-    <script src="/presale/assets/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="/presale/assets/js/material.min.js" type="text/javascript"></script>
-
-    <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script> -->
-    <!-- Library for adding dinamically elements -->
-    <script src="/presale/assets/js/arrive.min.js" type="text/javascript"></script>
-    <!-- Forms Validations Plugin -->
-    <script src="/presale/assets/js/jquery.validate.min.js"></script>
-    <!--  Plugin for Date Time Picker and Full Calendar Plugin-->
-    <script src="/presale/assets/js/moment.min.js"></script>
-    <!--  Charts Plugin, full documentation here: https://gionkunz.github.io/chartist-js/ -->
-    <script src="/presale/assets/js/chartist.min.js"></script>
-    <!--  Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
-    <script src="/presale/assets/js/jquery.bootstrap-wizard.js"></script>
-    <!--  Notifications Plugin, full documentation here: http://bootstrap-notify.remabledesigns.com/    -->
-    <script src="/presale/assets/js/bootstrap-notify.js"></script>
-    <!--   Sharrre Library    -->
-    <script src="/presale/assets/js/jquery.sharrre.js"></script>
-    <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
-    <script src="/presale/assets/js/bootstrap-datetimepicker.js"></script>
-    <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
-    <script src="/presale/assets/js/jquery-jvectormap.js"></script>
-    <!-- Sliders Plugin, full documentation here: https://refreshless.com/nouislider/ -->
-    <script src="/presale/assets/js/nouislider.min.js"></script>
-    <!--  Google Maps Plugin    -->
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD1_8C5Xz9RpEeJSaJ3E_DeBv8i7j_p6Aw"></script>
-    <!--  Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
-    <script src="/presale/assets/js/jquery.select-bootstrap.js"></script>
-    <!--  DataTables.net Plugin, full documentation here: https://datatables.net/    -->
-    <script src="/presale/assets/js/jquery.datatables.js"></script>
-    <!-- Sweet Alert 2 plugin, full documentation here: https://limonte.github.io/sweetalert2/ -->
-    <script src="/presale/assets/js/sweetalert2.js"></script>
-    <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
-    <script src="/presale/assets/js/jasny-bootstrap.min.js"></script>
-    <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
-    <script src="/presale/assets/js/fullcalendar.min.js"></script>
-    <!-- Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
-    <script src="/presale/assets/js/jquery.tagsinput.js"></script>
-    <!-- Material Dashboard javascript methods -->
-
-    <script src="/presale/assets/js/jquery.countdown.min.js"></script>
-    <!-- Material Dashboard Countdown -->
-
-    <!-- Material Dashboard DEMO methods, don't include it in your project! -->
-    <script src="/presale/assets/js/demo.js"></script>
-    <script type="text/template" id="main-example-template">
-
-        <div class="time <%= label %>">
-      <span class="count curr top"><%= curr %></span>
-      <span class="count next top"><%= next %></span>
-      <span class="count next bottom"><%= next %></span>
-      <span class="count curr bottom"><%= curr %></span>
-      <span class="label"><%= label.length < 6 ? label : label.substr(0, 3)  %></span>
-    </div>
-
-</script>
-<script type="text/javascript">
-$(document).ready(function() {
-
-    // Javascript method's body can be found in assets/js/demos.js
-    demo.initDashboardPageCharts();
-    demo.initCoutdown();
-
-});
-</script>
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js"></script>
     <script>
         $(document).ready(function () {
