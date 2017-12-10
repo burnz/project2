dcb9e760c85aa3dc0b6fd81b3bfa03398603565e
Tu Nguyen <tu.nguyen@bigin.vn>
2017-12-10 10:00:50 +0700

    handle_error_pages

--- resources/views/vendor/adminlte/errors/401.blade.php
+++ resources/views/vendor/adminlte/errors/401.blade.php
@@ -1,19 +1,17 @@
 @extends('adminlte::layouts.errors')
-
-@section('htmlheader_title')
-    {{ trans('adminlte_lang::message.servererror') }}
-@endsection
-
-@section('main-content')
-
-    <div class="error-page">
-        <h2 class="headline text-red">401</h2>
-        <div class="error-content">
-            <h3><i class="fa fa-warning text-red"></i> Oops! {{ trans('adminlte_lang::message.somethingwrong') }}</h3>
-            <p>
-                {{ trans('adminlte_lang::message.wewillwork') }}
-                {{ trans('adminlte_lang::message.mainwhile') }} <a href='{{ url('/home') }}'>{{ trans('adminlte_lang::message.returndashboard') }}</a>
-            </p>
-        </div>
-    </div><!-- /.error-page -->
+@section('title')
+{{ trans('adminlte_lang::message.somethingwrong') }}
+@stop
+@section('content')
+    <div class="main">
+        <h3>
+            401
+        </h3>
+        <h1> Oops! {{ trans('adminlte_lang::message.somethingwrong') }}</h1>
+        <p>
+            {{ trans('adminlte_lang::message.wewillwork') }}   
+            <br/> 
+            <span>{{ trans('adminlte_lang::message.mainwhile') }} <a href='{{ url('/home') }}'>{{ trans('adminlte_lang::message.returndashboard') }}</a></span>
+        </p> 
+    </div>
 @endsection
