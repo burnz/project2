c04b55df0dd3b4459e5665c9b451c7f85b7a6794
Tu Nguyen <tu.nguyen@bigin.vn>
2018-01-10 17:52:29 +0700

    fix bugs

--- public/Carcoin/js/demo.js
+++ public/Carcoin/js/demo.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
     
+    var ua = navigator.userAgent,
+    iOS = /iPad|iPhone|iPod/.test(ua),
+    iOS11 = /OS 11_0_1|OS 11_0_2|OS 11_0_3|OS 11_1|OS 11_1_1|OS 11_1_2|OS 11_2|OS 11_2_1/.test(ua);
+
+    // ios 11 bug caret position
+    if ( iOS && iOS11 ) {
+
+        // Add CSS class to body
+        $("body").addClass("iosBugFixCaret");
+
+    }
+    
     $("input[type=number]").on("keypress", function(event){
         var charCode = event.charCode;
         if (charCode != 0) {
--- resources/views/vendor/adminlte/wallets/clp.blade.php
+++ resources/views/vendor/adminlte/wallets/clp.blade.php
@@ -120,7 +120,7 @@
                                             <div class="clearfix"></div>
                                             <!-- <h4 class="card-title">Command</h4> -->
 
-                                            <div class="material-datatables">
+                                            <div class="material-datatables table-responsive">
                                                 <table class="table table-striped table-no-bordered table-hover" id="tbCLP" cellspacing="0" width="100%" style="width:100%">
                                                     <thead class="text-thirdary">
                                                         <th style="width: 20%;">Date/Time</th>
