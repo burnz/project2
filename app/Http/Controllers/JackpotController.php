80da775de25cfe8caffc8504dd12545873a04dd7
Giang <giangitman@gmail.com>
2018-07-25 01:34:07 +0700

    Update API for csc

--- app/Http/Controllers/JackpotController.php
+++ app/Http/Controllers/JackpotController.php
@@ -21,23 +21,12 @@ class JackpotController extends Controller
         {
             if($request->isMethod('post'))
             {
-                $accessToken = $request->first_token;
-                $secondToken = $request->second_token;
+                $accessToken = $request->api_key;
                 $email = trim($request->email);
                 $referralEmail = trim($request->referral_email);
-                $firstname = trim($request->firstname);
-                $lastnname = trim($request->lastname);
-                $phone = trim($request->phone);
-                $countryCode = trim($request->country_code);
-                $countryName = trim($request->country_name);
 
                 if($accessToken != config('app.access_token')) {
                     return response()->json(['result' => '0', 'error_msg' => 'Access token is invalid'], 500);
-                }
-
-                $hash = md5($email . config('app.salt'));
-                if($hash != strtolower($secondToken)) {
-                    return response()->json(['result' => '0', 'error_msg' => 'Validation token is invalid'], 500);
                 }
 
                 if($email == null){
@@ -67,18 +56,11 @@ class JackpotController extends Controller
                 $referral = User::where('email', $referralEmail)->first();
                 $password = Str::random();
 
-
                 $fields = [
-                        'firstname'         => $firstname,
-                        'lastname'          => $lastnname,
-                        //'password'          => bcrypt(12),
-                        'password'          => bcrypt($password),
                         'name'              => $name,
                         'email'             => $email,
+                        'password'          => bcrypt($password),
                         'refererId'         => isset($referral) ? $referral->id : 3042, //cscglobal
-                        'phone'             => $phone,
-                        'country'           => $countryCode,
-                        'name_country'      => $countryName,
                         'active'            => 1,
                         'uid'               => User::getUid(),
                         'google2fa_secret'  => Google2FA::generateSecretKey(16)
@@ -106,7 +88,6 @@ class JackpotController extends Controller
             }
 
         }catch (\Exception $e){
-
             return response()->json(['result' => '0', 'error_msg' => 'Application have error'], 500);
         }
     }
@@ -115,94 +96,85 @@ class JackpotController extends Controller
     {
         if($request->isMethod('post'))
         {
-            $accessToken = $request->first_token;
-            $secondToken = $request->second_token;
-            $email = trim($request->email);
-            $ticket = $request->num_of_ticket;
-            $dateTime = $request->date_time;
-
-
-            if($email == null){
-                return response()->json(['result' => '0', 'error_msg' => 'email is required'], 500);
-            }
-
-
-            if($ticket == null){
-                return response()->json(['result' => '0', 'error_msg' => 'num_of_ticket is required'], 500);
-            }
-
-
-            if($dateTime == null){
-                return response()->json(['result' => '0', 'error_msg' => 'date_time is required'], 500);
-            }
-
+            $accessToken = $request->api_key;
+            $emailTicketList = json_decode($request->data);
 
             if($accessToken != config('app.access_token')) {
                 return response()->json(['result' => '0', 'error_msg' => 'Access token is invalid'], 500);
             }
 
-            $hash = md5($email . $ticket . config('app.salt'));
-            if($hash != strtolower($secondToken)) {
-                return response()->json(['result' => '0', 'error_msg' => 'Validation token is invalid'], 500);
-            }
+            $successList = [];
+            foreach($emailTicketList as $item)
+            {
+                //Update ticket
+                try {
+                    $year = date('Y');
+                    
+                    $dt = Carbon::parse($item->date_time);
+                    $weeked = $dt->weekOfYear;
+                    //neu la CN thi day la ve cua tuan moi
+                    if($dt->dayOfWeek == 0){
+                        $weeked = $weeked + 1;
+                    }
 
+                    //neu la thu 7 nhung qua 9h thi day la ve cua tuan moi
+                    if($dt->dayOfWeek == 6 && $dt->hour > 8){
+                        $weeked = $weeked + 1;
+                    }
 
-            //Update ticket
-            try {
-                $year = date('Y');
-                
-                $dt = Carbon::parse($dateTime);
-                $weeked = $dt->weekOfYear;
-                //neu la CN thi day la ve cua tuan moi
-                if($dt->dayOfWeek == 0){
-                    $weeked = $weeked + 1;
-                }
+                    if($weeked == 53) {
+                        $weeked = 1;
+                        $year += 1;
+                    }
 
-                //neu la thu 7 nhung qua 9h thi day la ve cua tuan moi
-                if($dt->dayOfWeek == 6 && $dt->hour > 8){
-                    $weeked = $weeked + 1;
-                }
+                    $weekYear = $year . $weeked;
 
-                if($weeked == 53) {
-                    $weeked = 1;
-                    $year += 1;
-                }
+                    //get user id from name
+                    $oUser = User::where('email', $item->email)->first();
 
-                $weekYear = $year . $weeked;
+                    //if not exist user continue check next item
+                    if($oUser == null) continue;
 
-                //get user id from name
-                $oUser = User::where('email', $email)->first();
-
-                $oTicket = Tickets::where('user_id', $oUser->id)->where('week_year', $weekYear)->first();
-                if(isset($oTicket)) {
-                    $oTicket->personal_quantity += $ticket;
-                    $oTicket->quantity += $ticket;
-                    $oTicket->save();
-
-                } else {
-                    $field = ['user_id' => $oUser->id, 'week_year' => $weekYear, 'personal_quantity' => $ticket, 'quantity' => $ticket];
-                    Tickets::create($field);
-                }
-
-                //Update doanh so cho dai ly
-                if($oUser->userData->packageId == 0) {
-                    //get id agency
-                    $agencyId = User::_getAgency($oUser->id);
-                    $oTicket = Tickets::where('user_id', $agencyId)->where('week_year', $weekYear)->first();
+                    $oTicket = Tickets::where('user_id', $oUser->id)->where('week_year', $weekYear)->first();
                     if(isset($oTicket)) {
-                        $oTicket->quantity += $ticket;
+                        $oTicket->personal_quantity += $item->ticket;
+                        $oTicket->quantity += $item->ticket;
                         $oTicket->save();
+
                     } else {
-                        $field = ['user_id' => $agencyId, 'week_year' => $weekYear, 'quantity' => $ticket];
+                        $field = [
+                                'user_id' => $oUser->id, 
+                                'week_year' => $weekYear, 
+                                'personal_quantity' => $item->ticket, 
+                                'quantity' => $item->ticket
+                            ];
                         Tickets::create($field);
                     }
-                }
 
-            } catch (\Exception $e) {
-                return response()->json(['result' => '0', 'error_msg' => 'Somethings wrong'], 500);
+                    //Update doanh so cho dai ly
+                    if($oUser->userData->packageId == 0) {
+                        //get id agency
+                        $agencyId = User::_getAgency($oUser->id);
+                        $oTicket = Tickets::where('user_id', $agencyId)->where('week_year', $weekYear)->first();
+                        if(isset($oTicket)) {
+                            $oTicket->quantity += $item->ticket;
+                            $oTicket->save();
+                        } else {
+                            $field = ['user_id' => $agencyId, 'week_year' => $weekYear, 'quantity' => $item->ticket];
+                            Tickets::create($field);
+                        }
+                    }
+
+                    //add to success list
+                    $successList[] = ['id' => $item->id];
+                } catch (\Exception $e) {
+                    \Log::info($e->getMessage());
+                    \Log::info($e->getTraceAsString());
+                    return response()->json(['result' => '0', 'error_msg' => 'Somethings wrong'], 500);
+                }
             }
 
-            return response()->json(['result' => '1', 'email' => $email, 'num_of_ticket' => $ticket], 200);
+            return response()->json(['result' => '1', 'sucess_list' => $successList], 200);
         } else {
             return response()->json(['result' => '0', 'error_msg' => 'Request is invalid'], 500);
         }
@@ -212,93 +184,69 @@ class JackpotController extends Controller
     {
         if($request->isMethod('post'))
         {
-            $accessToken = trim($request->first_token);
-            $secondToken = trim($request->second_token);
-            $email = trim($request->email);
-            $award = trim($request->value);
-            $dateTime = trim($request->date_time);
-
-            
-            if($email == null){
-                return response()->json(['result' => '0', 'error_msg' => 'email is required'], 500);
-            }
-
-
-            if($award == null){
-                return response()->json(['result' => '0', 'error_msg' => 'value is required'], 500);
-            }
-
-
-            if($dateTime == null){
-                return response()->json(['result' => '0', 'error_msg' => 'date_time is required'], 500);
-            }
-
+            $accessToken = trim($request->api_key);
+            $emailWinningList = json_decode($request->data);
 
             if($accessToken != config('app.access_token')) {
                 return response()->json(['result' => '0', 'error_msg' => 'Access token is invalid'], 500);
             }
 
-            $hash = md5($email . $award . config('app.salt'));
-            if($hash != strtolower($secondToken)) {
-                return response()->json(['result' => '0', 'error_msg' => 'Validation token is invalid'], 500);
-            }
+            $successList = [];
+            foreach($emailWinningList as $item)
+            {
+                //Update award
+                try {
+                    $year = date('Y');
+                    
+                    $dt = Carbon::parse($item->date_time);
+                    $weeked = $dt->weekOfYear;
+                    
+                    $weekYear = $year . $weeked;
 
-            //Update award
-            try {
-                $year = date('Y');
-                
-                $dt = Carbon::parse($dateTime);
-                $weeked = $dt->weekOfYear;
-                //neu la CN thi day la ve cua tuan moi
-                if($dt->dayOfWeek == 0){
-                    $weeked = $weeked + 1;
-                }
+                    //get user id from name
+                    $oUser = User::where('email', $item->email)->first();
 
-                //neu la thu 7 nhung qua 9h thi day la ve cua tuan moi
-                if($dt->dayOfWeek == 6 && $dt->hour > 8){
-                    $weeked = $weeked + 1;
-                }
+                    //if not exist user continue check next item
+                    if($oUser == null) {
+                        \Log::info('winning ' . $item->email . ' not exits');
+                        continue;
+                    }
 
-                if($weeked == 53) {
-                    $weeked = 1;
-                    $year += 1;
-                }
-
-                $weekYear = $year . $weeked;
-
-                //get user id from name
-                $oUser = User::where('email', $email)->first();
-
-                $oTicket = Awards::where('user_id', $oUser->id)->where('week_year', $weekYear)->first();
-                if(isset($oTicket)) {
-                    $oTicket->personal_value += $award;
-                    $oTicket->value += $award;
-                    $oTicket->save();
-
-                } else {
-                    $field = ['user_id' => $oUser->id, 'week_year' => $weekYear, 'personal_value' => $award, 'value' => $award];
-                    Awards::create($field);
-                }
-
-                //Update doanh so cho dai ly
-                if($oUser->userData->packageId == 0) {
-                    //get id agency
-                    $agencyId = User::_getAgency($oUser->id);
-                    $oTicket = Awards::where('user_id', $agencyId)->where('week_year', $weekYear)->first();
+                    $oTicket = Awards::where('user_id', $oUser->id)->where('week_year', $weekYear)->first();
                     if(isset($oTicket)) {
-                        $oTicket->value += $award;
+                        $oTicket->personal_value += $item->value;
+                        $oTicket->value += $item->value;
                         $oTicket->save();
+
                     } else {
-                        $field = ['user_id' => $agencyId, 'week_year' => $weekYear, 'value' => $award];
+                        $field = ['user_id' => $oUser->id, 'week_year' => $weekYear, 'personal_value' => $item->value, 'value' => $item->value];
                         Awards::create($field);
                     }
-                }
 
-            } catch (\Exception $e) {
-                return response()->json(['result' => '0', 'error_msg' => 'Somethings wrong'], 500);
+                    //Update doanh so cho dai ly
+                    if($oUser->userData->packageId == 0) {
+                        //get id agency
+                        $agencyId = User::_getAgency($oUser->id);
+                        $oTicket = Awards::where('user_id', $agencyId)->where('week_year', $weekYear)->first();
+                        if(isset($oTicket)) {
+                            $oTicket->value += $item->value;
+                            $oTicket->save();
+                        } else {
+                            $field = ['user_id' => $agencyId, 'week_year' => $weekYear, 'value' => $item->value];
+                            Awards::create($field);
+                        }
+                    }
+
+                    //add to success list
+                    $successList[] = ['id' => $item->id];
+                } catch (\Exception $e) {
+                    \Log::info($e->getMessage());
+                    \Log::info($e->getTraceAsString());
+                    return response()->json(['result' => '0', 'error_msg' => 'Somethings wrong'], 500);
+                }
             }
 
-            return response()->json(['result' => '1', 'email' => $email, 'value' => $award], 200);
+            return response()->json(['result' => '1', 'sucess_list' => $successList], 200);
         } else {
             return response()->json(['result' => '0', 'error_msg' => 'Request is invalid'], 500);
         }
